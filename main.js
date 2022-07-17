75215c6584a8c260317e998c8e0934c40aaf5c63
POOM <nitipoom_t@cmu.ac.th>
2022-07-17 13:52:59 +0700

    3.1

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
+            brand: 'SE 331',
             image: './assets/images/socks_green.jpg',
             inStock: false,
             inventory: 100,
